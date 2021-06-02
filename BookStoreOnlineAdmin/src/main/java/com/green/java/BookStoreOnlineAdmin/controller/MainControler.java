ece457f981aac2a9811041974fe1dfe459b538d2
add-codes <add762603@gmail.com>
2021-06-02 12:42:30 +0700

    + Fourth commit

--- BookStoreOnlineAdmin/src/main/java/com/green/java/BookStoreOnlineAdmin/controller/MainControler.java
+++ BookStoreOnlineAdmin/src/main/java/com/green/java/BookStoreOnlineAdmin/controller/MainControler.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class MainControler {
 
-	@GetMapping("/login")
+	@GetMapping("/loginForm")
 	public String showLoginView() {
 		return "login";
 	}	
