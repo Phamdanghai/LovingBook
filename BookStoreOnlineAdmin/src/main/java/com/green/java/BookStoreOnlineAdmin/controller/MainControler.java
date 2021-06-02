50194b5cc62e018167eb75a167c4ca3967943c76
add-codes <add762603@gmail.com>
2021-06-02 10:52:55 +0700

    + Third commit
    - Created Admin Login page using HTML and CSS

--- BookStoreOnlineAdmin/src/main/java/com/green/java/BookStoreOnlineAdmin/controller/MainControler.java
+++ BookStoreOnlineAdmin/src/main/java/com/green/java/BookStoreOnlineAdmin/controller/MainControler.java
@@ -1,8 +1,15 @@
 package com.green.java.BookStoreOnlineAdmin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainControler {
+
+	@GetMapping("/login")
+	public String showLoginView() {
+		return "login";
+	}	
 
 }
